da44456835df07c39471959ee2014d21b1b2f988
Marcos Tulio Gomes da Silva Junior <mtgsjr@hotmail.com>
2021-05-29 20:08:05 -0300

    API REST V.1.0

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -4,7 +4,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
+use App\Entity\Course;
 
 /**
  * @Route("/course", name="course_")
@@ -31,23 +34,63 @@ class CourseController extends AbstractController
     // Vai listar apenas um
     public function show($courseId)
     {
+        $course = $this->getDoctrine()->getRepository(Course::class)->find($courseId);
+        
+        return $this->json([
+            'data' => $course
+        ]);
 
     }
 
      /**
      * @Route("/", name="create", methods={"POST"})
      */
-    public function create()
+    public function create(Request $request): Response
     {
+        $dados = $request->request->all();
+
+        $course = new Course();
         
+        $course->setName($dados['name']);
+        $course->setDescription($dados['description']);
+        $course->setSlug($dados['slug']);
+        $course->setCreatedAt(new \DateTime('now', new \DateTimezone('America/Recife')));
+        $course->setUpdatedAt(new \DateTime('now', new \DateTimezone('America/Recife')));
+        
+        $doctrine = $this->getDoctrine()->getManager();
+        $doctrine->persist($course);
+        $doctrine->flush();
+
+        return $this->json($dados);
     }
 
      /**
      * @Route("/{courseId}", name="update", methods={"PUT", "PATCH"})
      */
-    public function update($courseId)
+    
+     // PUT atualiza tudo, PATCH atualiza alguns campos
+
+    public function update($courseId, Request $request)
     {
+        $dados = $request->request->all();
+
+        $course = $this->getDoctrine()->getRepository(Course::class)->find($courseId);
         
+        if ($request->request->has('name'))
+            $course->setName($dados['name']);
+        
+        if ($request->request->has('description'))    
+            $course->setDescription($dados['description']);
+         
+        if ($request->request->has('slug'))    
+            $course->setSlug($dados['slug']);
+
+        $course->setUpdatedAt(new \DateTime('now', new \DateTimezone('America/Recife')));
+        
+        $doctrine = $this->getDoctrine()->getManager();        
+        $doctrine->flush();
+
+        return $this->json($dados);
     }
 
      /**
@@ -55,7 +98,14 @@ class CourseController extends AbstractController
      */
     public function delete($courseId)
     {
+        $course = $this->getDoctrine()->getRepository(Course::class)->find($courseId);
+        $doctrine = $this->getDoctrine()->getManager();
+        $doctrine->remove($course);
+        $doctrine->flush();
         
+        return $this->json([
+            'data' => 'Curso Removido'
+        ]);
     }
 
 
